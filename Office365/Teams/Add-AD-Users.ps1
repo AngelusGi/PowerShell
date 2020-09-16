ccfe64cba978152ab1864e405178945511588418
Angelo Gino Varrati <angelo.varrati@outlook.it>
2020-09-16 21:55:25 +0200

    add user to teams

---
+++ Office365/Teams/Add-AD-Users.ps1
@@ -0,0 +1,29 @@
+# PARAMETERS #
+
+$PathCSV = "<YOUR CSV PATH>" # ex. ".\csv_test.CSV" # Modificare inserendo la path e il nome del file CSV che contiene gli utenti da inserire
+
+$Delimiter = "<YOUR DELIMITER IN THE CSV FILE>" # ex. ';' # Delimitatore del file CSV
+
+$TeamName = "<TEAM NAME>" # ex. "Team Contoso"
+
+$Role = "Member" # the deafult member type is "Member", isted of this, you can set it as "Owner"
+
+# END PARAMETERS #
+
+
+Install-Module MicrosoftTeams
+Import-Module MicrosoftTeams
+
+Connect-MicrosoftTeams
+
+$Team = Get-Team -DisplayName $TeamName
+
+$Users = Import-Csv $PathCSV -Delimiter $Delimiter
+
+$Users | ForEach-Object {
+
+    Add-TeamUser -GroupId $Team.GroupId -User $_.Email -Role $Role
+
+    Write-Warning("*** Operazione compeltata su '$($_.Email)' nel team '$($Team.DisplayName)' ***")
+
+}
