da9506b0b249cb782c8645fd0330598077513d1a
Angelo Gino Varrati <v-anvaratti@microsoft.com>
2021-01-18 18:38:49 +0100

    script update

--- Office365/Teams/Attendance Report/ActivateAttendaceReport.ps1
+++ Office365/Teams/Attendance Report/ActivateAttendaceReport.ps1
@@ -1,82 +1,120 @@
 
-$OrganizerOnly = 1
-$NotOnlyOrganizer = 2
+# Region module manager
 
-Write-Warning("Verifica dell'ambiente in corso, attendere...")
+function CheckModules {
+    param (
+        $Modules,
+        [String]$Scope
+    )
+    process {
 
-if ($PSVersionTable.PSVersion.Major -ne 5) {
-    Write-Error("Questo script può essere eseguito solo con la versione 5 di Windows PowerShell")
-    exit
+        foreach ($module in $Modules) {
+            
+            $GalleryModule = Find-Module -Name $module
+
+            $mod = $installedModules | Where-Object { $_.Name -eq $module }
+        
+            if ([string]::IsNullOrEmpty($mod) -or [string]::IsNullOrWhiteSpace($mod)) {
+                Write-Warning("Modulo $($module) non trovato. Installazione in corso...")
+                Install-Module -Name $module -Scope $Scope
+            }
+            else {
+                Write-Warning("Modulo $($module) trovato.")
+
+                if ($GalleryModule.Version -ne $mod.Version) {
+                    Write-Warning("Aggionamento del modulo $($module) in corso...")
+
+                    Update-Module -Name $module -Force
+                }
+            }
+            Import-Module -Name $module
+
+        }
+
+        
+    }
 }
 
-$modName = "MicrosoftTeams"
+function VerifyPsVersion {
+    
+    process {
+        
+        Write-Warning("Verifica dell'ambiente in corso, attendere...")
 
-$mod = Get-InstalledModule -Name $modName
-
-if ([string]::IsNullOrEmpty($mod) -or [string]::IsNullOrWhiteSpace($mod)) {
-    Write-Error("Modulo $($modName) non trovato. Installazione in corso...")
-    Install-Module -Name MicrosoftTeams -RequiredVersion 1.1.6 -Scope CurrentUser
+        if ($PSVersionTable.PSVersion.Major -ne 5) {
+            Write-Error("Questo script può essere eseguito solo con la versione 5 di Windows PowerShell")
+            exit
+        }
+    }
 }
 
-$mod_version = $mod.Version.ToString()
-$older = 1
+function InstallLocalModules {
+    param (
+        [String]$Scope,
+        $Modules
+    )
 
-if ($mod_version.CompareTo("1.1.6") -eq $older) {
-    Write-Error("Aggiornamento in corso del modulo $($modName), versione attualmente in uso $($mod_version) ...")
-    Update-Module -Name $modName -Scope CurrentUser -Force
+    process {
+
+        VerifyPsVersion
+
+        CheckModules -Modules $Modules -Scope $Scope
+        
+    }
     
 }
 
+function ExitSessions {
 
-Import-Module -Name $modName
+    process {
+        Disconnect-MicrosoftTeams -Confirm:$false
+    }
+    
+}
 
-Write-Output("Modulo attualmente in uso:")
-Write-Output($mod)
-Write-Output("")
-Write-Output("")
+# EndRegion
 
-Write-Output("Inserisci il numero inerente la policy che vuoi applicare a livello di tenant:")
-Write-Output("$($OrganizerOnly). download attendance report disponibile solo per chi ha programmato la riunione")
-Write-Output("$($NotOnlyOrganizer). download attendance report disponibile per tutti i relatori e partecipanti nel meeting")
+InstallLocalModules -Modules "MicrosoftTeams" -Scope "CurrentUser"
 
-$response = Read-Host("Inserisci 1 o 2 e premi INVIO")
+Write-Warning("Inserire le credenziali dell'amministratore del tanant")
 
-Write-Output("")
-Write-Output("")
+try {
+    $adminCred = Get-Credential
+    $session = New-CsOnlineSession -Credential $adminCred
+    Connect-MicrosoftTeams -Credential $adminCred 
+    Import-PSSession $session -AllowClobber
 
-if ([string]::IsNullOrEmpty($response) -or [string]::IsNullOrWhiteSpace($response)) {
-    Write-Error("Valore non corretto.")
+    
+}
+catch {
+    Write-Warning("Autenticazione non riuscita, verificare le credenziali inserite.")
     exit
 }
 
-Write-Warning("Inserire le credenziali dell'amministratore del tanant nella finestra che si è appena aperta")
+$OrganizerOnly = 1
+$NotOnlyOrganizer = 2
 
+do {
+    Clear-Host
 
-$adminCred = Get-Credential
+    Write-Host("Inserisci il numero inerente la policy che vuoi applicare a livello di tenant:")
+    Write-Host("$($OrganizerOnly). download attendance report disponibile solo per chi ha programmato la riunione")
+    Write-Host("$($NotOnlyOrganizer). download attendance report disponibile per tutti i relatori e partecipanti nel meeting")
+    
+    $response = Read-Host("Inserisci 1 o 2 e premi INVIO")
 
-if($null -eq $adminCred){
-    Write-Warning("Credenziali errate, riprovare")
-    exit
-}
+    Write-Host("")
 
-$session = New-CsOnlineSession -Credential $adminCred
+    
+} while (-not (($OrganizerOnly -eq $response) -or ($NotOnlyOrganizer -eq $response)))
 
-if($null -eq $session){
-    Write-Warning("Credenziali errate, riprovare")
-    exit
-}
-
-Connect-MicrosoftTeams -Credential $adminCred 
-Import-PSSession $session -AllowClobber
-
-
+$msg = $null
 
 if ($OrganizerOnly -eq $response) {
 
     # download report solo per chi ha programmato la riunione
     Set-CsTeamsMeetingPolicy -AllowEngagementReport "Enabled"
     $msg = "Applicazione della policy 'Solo organizzatore' completata, la propagazione della modifica potrebbe richiedere fino a 24 ore."
-    Write-Warning($msg)
 
 }
 elseif ($NotOnlyOrganizer -eq $response) {
@@ -84,11 +122,11 @@ elseif ($NotOnlyOrganizer -eq $response) {
     # download report per tutti i relatori e partecipanti
     Set-CsTeamsMeetingPolicy -Identity Global -AllowEngagementReport "Enabled"
     $msg = "Applicazione della policy 'tutti i relatori e partecipanti' completata, la propagazione della modifica potrebbe richiedere fino a 24 ore."
-    Write-Warning($msg)
 
 }
-else {
-    Write-Error("Scelta corretta. Nessuna modifica apportata al sistema.")
-}
 
-Write-Output("*** Esecuzione completata ***")
+Write-Warning($msg)
+
+ExitSessions
+
+Write-Host("*** Esecuzione completata ***")
