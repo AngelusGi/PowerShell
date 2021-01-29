c95a575fcbba8fe33da0cab0dbe33cd2ac04380b
Angelo Gino Varrati <v-anvaratti@microsoft.com>
2021-01-29 19:50:04 +0100

    fix

--- Tools/ModuleManager.ps1
+++ Tools/ModuleManager.ps1
@@ -13,8 +13,8 @@ param(
 
     # Version required to run the script - if null every version of PowerShell is good
     [Parameter()]
-    [String]
-    $CompatibleVersion = $null
+    [int16]
+    $CompatibleVersion = 0
 )
 
 function CheckModules {
@@ -24,32 +24,54 @@ function CheckModules {
     )
     process {
 
-        if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
-            Write-Error -Message ('Il modulo AzureRM è installato sulla macchina. Rimuoverlo prima di procedere.')
-            Break
-        }
-
         $installedModules = Get-InstalledModule
 
         foreach ($module in $Modules) {
-            
-            $GalleryModule = Find-Module -Name $module
-
-            $mod = $installedModules | Where-Object { $_.Name -eq $module }
         
-            if ([string]::IsNullOrEmpty($mod) -or [string]::IsNullOrWhiteSpace($mod)) {
-                Write-Warning("Modulo $($module) non trovato. Installazione in corso...")
-                Install-Module -Name $module -Scope $Scope -AllowClobber
-            }
-            else {
-                Write-Warning("Modulo $($module) trovato.")
-
-                if ($GalleryModule.Version -ne $mod.Version) {
-                    Write-Warning("Aggionamento del modulo $($module) in corso...")
-
-                    Update-Module -Name $module
+            if ($module -eq "Az") {
+                if ($PSVersionTable.PSEdition -eq 'Desktop' -and (Get-Module -Name AzureRM -ListAvailable)) {
+                    throw 'Il modulo AzureRM è installato sulla macchina. Rimuoverlo prima di procedere.'
                 }
             }
+
+            if ($module -eq "Az.LabServices") {
+                
+                Write-Warning("Installazione del modulo Az.LabServices in corso...")
+
+                $LabServiceLibraryURL = "https://raw.githubusercontent.com/Azure/azure-devtestlab/master/samples/ClassroomLabs/Modules/Library/Az.LabServices.psm1"
+
+                $Client = New-Object System.Net.WebClient
+
+                $Client.DownloadFile($LabServiceLibraryURL, ".\Az.LabServices.psm1")
+
+                Import-Module .\Az.LabServices.psm1
+
+            } else {
+                $GalleryModule = Find-Module -Name $module
+
+                $mod = $installedModules | Where-Object { $_.Name -eq $module }
+            
+                if ([string]::IsNullOrEmpty($mod) -or [string]::IsNullOrWhiteSpace($mod)) {
+                    Write-Warning("Modulo $($module) non trovato. Installazione in corso...")
+                    Install-Module -Name $module -Scope $Scope -AllowClobber
+                }
+                else {
+                    Write-Warning("Modulo $($module) trovato.")
+    
+                    if ($GalleryModule.Version -ne $mod.Version) {
+                        Write-Warning("Aggionamento del modulo $($module) in corso...")
+    
+                        Update-Module -Name $module
+                    }
+                }
+
+                Import-Module $module
+                Write-Output("Modulo $($module) importato correttamente")
+            }
+
+        
+
+            
         }
 
        
@@ -63,10 +85,9 @@ function VerifyPsVersion {
         
         Write-Warning("Verifica dell'ambiente in corso, attendere...")
 
-        if ($null -ne $CompatibleVersion) {
+        if (0 -ne $CompatibleVersion) {
             if ($PSVersionTable.PSVersion.Major -ne $CompatibleVersion) {
-                Write-Error("Questo script può essere eseguito solo con la versione $($CompatibleVersion) di PowerShell")
-                exit
+                throw "Questo script può essere eseguito solo con la versione $($CompatibleVersion) di PowerShell"
             }
         }
         
