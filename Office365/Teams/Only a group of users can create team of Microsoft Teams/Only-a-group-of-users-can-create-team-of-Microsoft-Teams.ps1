9a1c140e17ed0d88632026e0677dbd7d1e0abe50
Angelo Gino Varrati <v-anvaratti@microsoft.com>
2021-03-15 10:10:15 +0100

    new name

---
+++ Office365/Teams/Only a group of users can create team of Microsoft Teams/Only-a-group-of-users-can-create-team-of-Microsoft-Teams.ps1
@@ -0,0 +1,83 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [String]
+    $GroupName
+)
+
+
+# Region module manager
+
+function ExitSessions {
+
+    process {
+        Disconnect-AzureAD -Confirm:$false
+    }
+    
+}
+
+
+function PrepareEnvironment {
+
+    param(
+        [Parameter(Mandatory = $true)]
+        [String[]]
+        $Modules,
+        [int16]
+        $Version
+    )
+    
+    process {
+
+        $LibraryURL = "https://raw.githubusercontent.com/AngelusGi/PowerShell/master/Tools/ModuleManager.ps1"
+
+        $Client = New-Object System.Net.WebClient
+    
+        $currentPath = Get-Location
+
+        $downloadPath = $currentPath.Path + "\ModuleManager.ps1"
+        
+        $Client.DownloadFile($LibraryURL, $downloadPath)
+
+        .\ModuleManager.ps1 -Modules $Modules -CompatibleVersion $Version 
+
+    }
+    
+}
+
+
+# End parameters region
+
+
+PrepareEnvironment -Modules "AzureAdPreview"
+
+$AllowGroupCreation = $False
+
+Connect-AzureAD
+
+$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+if(!$settingsObjectID)
+{
+    $template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
+    $settingsCopy = $template.CreateDirectorySetting()
+    New-AzureADDirectorySetting -DirectorySetting $settingsCopy
+    $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+}
+
+$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
+$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
+
+if($GroupName)
+{
+  $settingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
+}
+ else {
+$settingsCopy["GroupCreationAllowedGroupId"] = $GroupName
+}
+Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
+
+(Get-AzureADDirectorySetting -Id $settingsObjectID).Values
+
+ExitSessions
+
+Write-Host("Esecuzione script completata.")
